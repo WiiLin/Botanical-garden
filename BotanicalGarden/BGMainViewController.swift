49ab18a4e4c4d3af33a1322cb247db6b783296be
Wii Lin <ya790504@gmail.com>
2021-04-12 15:48:10 +0800

    ♻️ controller rename to BGMainViewController

--- BotanicalGarden/BGMainViewController.swift
+++ BotanicalGarden/BGMainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BGMainViewController.swift
 //  BotanicalGarden
 //
 //  Created by Wii Lin on 2021/4/12.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BGMainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
