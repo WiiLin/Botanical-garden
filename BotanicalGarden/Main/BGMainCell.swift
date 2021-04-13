caf72714cea0694818527f2c28461e861f00656d
Wii Lin <ya790504@gmail.com>
2021-04-13 13:32:14 +0800

    🐛 屬性宣告調整

--- BotanicalGarden/Main/BGMainCell.swift
+++ BotanicalGarden/Main/BGMainCell.swift
@@ -19,6 +19,7 @@ class BGMainCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        coverImageView.kf.indicatorType = .activity
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -28,7 +29,6 @@ class BGMainCell: UITableViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         coverImageView.kf.cancelDownloadTask()
-        coverImageView.kf.indicatorType = .activity
     }
 
     // MARK: - Interface
